03260f52fea122450c9b144f42094cc8a2162826
Ramiro Foschni <foschniramiro@gmail.com>
2023-04-06 16:29:16 -0300

    v3

--- src/main/java/com/itr/controladores/JugadorControlador.java
+++ src/main/java/com/itr/controladores/JugadorControlador.java
@@ -1,5 +1,6 @@
 package com.itr.controladores;
 import com.itr.modelo.Jugador;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.itr.servicios.JugadorServicio;
@@ -21,27 +22,32 @@ this.jugadorServicio = jugadorServicio;
 
 
     @GetMapping
+    @Operation(summary = "Devuelve la lista de jugadores")
     public List<Jugador> listarJugadores (){
 
     return jugadorServicio.listarJugadores();
 }
     @GetMapping("/{id}")
+    @Operation(summary = "Devuelve un jugador por su ID")
     public Jugador obtenerPorId (@PathVariable Integer id){
         return jugadorServicio.obtenerPorId(id);
     }
 
     @PostMapping
+    @Operation(summary = "Crea un jugador")
     public void crearJugador(@RequestBody Jugador jugador) {
     jugadorServicio.crearJugador(jugador);
 
 }
     @PutMapping
+    @Operation(summary = "Actualiza un jugador")
     public void actualizar(@RequestBody Jugador jugador){
 
     jugadorServicio.actualizar(jugador);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina un jugador por su ID")
     public void eliminar (@PathVariable Integer id){
     jugadorServicio.eliminaJugador(id);
 
